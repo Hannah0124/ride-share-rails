70179c0046b9496edffc25b2b31cb02693f28a45
Hannah <hannahjoo24@gmail.com>
2020-05-07 14:43:17 -0700

    Updated Drivers Controller and views (_form, edit, index, new, show)

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,66 @@
+class DriversController < ApplicationController
+  def index 
+    @drivers = Driver.all
+  end 
+
+  def show 
+    driver_id = params[:id] 
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil? 
+      redirect_to root_path 
+      return 
+    end
+  end 
+
+  def new 
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+  end 
+
+  def edit 
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      redirect_to root_path 
+      return 
+    end
+  end 
+
+  def update 
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id) 
+
+    if @driver.nil? 
+      redirect_to root_path 
+      return 
+
+    elsif @driver.update(driver_params) 
+      redirect_to driver_path(@driver.id)
+      return 
+
+    else 
+      render :edit, :bad_request
+      return 
+    end
+  end
+
+  def destroy 
+    driver_id = params[:id] 
+    @driver = Driver.find_by(id: driver_id) 
+
+    if @driver.nil?
+      redirect_to root_path 
+      return 
+    else 
+      @driver.destroy 
+      redirect_to drivers_path 
+      return 
+    end
+  end
+end
+
