d1377b39f1d9002a11a2302bedf8bc269a3e6339
Hannah <hannahjoo24@gmail.com>
2020-05-07 14:35:39 -0700

    Updated Trips controller

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,69 @@
+class TripsController < ApplicationController
+
+  # def show 
+  #   trip_id = params[:id] 
+  #   @trip = Trip.find_by(id: trip_id)
+
+  #   if @trip.nil? 
+  #     redirect_to root_path 
+  #     return 
+  #   end
+  # end 
+
+  # def new 
+  #   @trip = Trip.new
+  # end
+
+  # def create
+  #   @trip = Trip.new(trip_params)
+  # end 
+
+  # def edit 
+  #   trip_id = params[:id]
+  #   @trip = Trip.find_by(id: trip_id)
+
+  #   if @trip.nil?
+  #     redirect_to root_path 
+  #     return 
+  #   end
+  # end 
+
+  # def update 
+  #   trip_id = params[:id]
+  #   @trip = Trip.find_by(id: trip_id) 
+
+  #   if @trip.nil? 
+  #     redirect_to root_path 
+  #     return 
+
+  #   elsif @trip.update(trip_params) 
+  #     redirect_to trip_path(@trip.id)
+  #     return 
+
+  #   else 
+  #     render :edit, :bad_request
+  #     return 
+  #   end
+  # end
+
+  # def destroy 
+  #   trip_id = params[:id] 
+  #   @trip = Trip.find_by(id: trip_id) 
+
+  #   if @trip.nil?
+  #     redirect_to root_path 
+  #     return 
+  #   else 
+  #     @trip.destroy 
+  #     redirect_to trips_path 
+  #     return 
+  #   end
+  # end
+
+  private 
+
+  def trip_params 
+    return params.require(:trip).permit( :driver_id, :passenger_id, :date, :rating, :cost)
+  end
+end
+
