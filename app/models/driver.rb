bb377100c098f8ce097cb28b700675566cf4455b
Hannah <hannahjoo24@gmail.com>
2020-05-08 12:52:26 -0700

    Added  in passenger.rb and driver.rb

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,5 @@
 class Driver < ApplicationRecord
-  has_many :trips
+  has_many :trips, dependent: :destroy
 
   def total_earnings 
     # The driver gets 80% of the trip cost after a fee of $1.65 is subtracted
@@ -7,8 +7,14 @@ class Driver < ApplicationRecord
   end
 
   def average_rating 
+    length = self.trips.count  
+
+    return 0 if length == 0 
+      
     total_rating = self.trips.sum { |trip| p trip.rating.to_f }
-    length = self.trips.count
+    
     return (total_rating / length).round(1)
   end
 end
+
+ # reference: https://github.com/jenseng/immigrant/issues/31
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,8 +1,10 @@
 class Passenger < ApplicationRecord
-  has_many :trips
+  has_many :trips, dependent: :destroy
 
   def total_charges 
     return self.trips.sum { |trip| p trip.cost.to_f }.round(2)
   end
 
 end
+
+  # reference: https://github.com/jenseng/immigrant/issues/31
