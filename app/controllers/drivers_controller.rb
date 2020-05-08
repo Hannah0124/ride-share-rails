124b7979fb268159b0e974799e32ba044917c140
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-07 21:56:41 -0700

    Fixed mistake in drivers_controller driver_params action

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -66,8 +66,8 @@ class DriversController < ApplicationController
   private
 
   def driver_params
-    return params.require(:driver).permit(:name, :vin, :available)
+    return params.require(:driver).permit(:name, :vin)
   end
-  
+
 end
 
