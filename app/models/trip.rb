97d8e37dacb429d099d7099dfed81daa4fcf43c7
Hannah <hannahjoo24@gmail.com>
2020-05-09 18:25:04 -0700

    Added trip validation

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,6 +4,8 @@ class Trip < ApplicationRecord
   belongs_to :passenger 
   belongs_to :driver
 
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
+
   def self.connect_trip
     driver = Driver.find_available_drivers 
     driver.update(available: false)
